25abf7a446b132debb890db2db509ce8e8524372
Gonzalo Lupo <lupohanamichi@gmail.com>
2021-07-15 19:31:23 -0300

    doce columnas y accessors

--- src/Components/Columns.js
+++ src/Components/Columns.js
@@ -5,35 +5,46 @@ export const COLUMNS = [
     },
     {
         Header: 'Comercio'
+        accessor: 'Comercio'
     },
     {
         Header: 'CUIT'
+        accessor: 'CUIT'
     },
     {
         Header: 'Concepto 1'
+        accessor: 'Concepto 1'
     },
     {
         Header: 'Concepto 2'
+        accessor: 'Concepto 2'
     },
     {
         Header: 'Concepto 3'
+        accessor: 'Concepto 3'
     },
     {
         Header: 'Concepto 4'
+        accessor: 'Concepto 4'
     },
     {
         Header: 'Concepto 5'
+        accessor: 'Concepto 5'
     },
     {
         Header: 'Concepto 6'
+        accessor: 'Concepto 6'
     },
     {
         Header: 'Balance actual'
+        accessor: 'Balance actual'
     },
     {
         Header: 'Activo'
+        accessor: 'Activo'
     },
     {
         Header: 'Última venta'
+        accessor: 'Última venta'
     },
 ]
