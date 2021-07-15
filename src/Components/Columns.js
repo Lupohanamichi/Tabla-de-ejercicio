bf36e773802bdd51a14280aa5bb4e698acbd874f
Gonzalo Lupo <lupohanamichi@gmail.com>
2021-07-15 19:22:17 -0300

    creación de columnas y primer accessor

---
+++ src/Components/Columns.js
@@ -0,0 +1,39 @@
+export const COLUMNS = [
+    {
+        Header: 'ID'
+        accessor: 'ID'
+    },
+    {
+        Header: 'Comercio'
+    },
+    {
+        Header: 'CUIT'
+    },
+    {
+        Header: 'Concepto 1'
+    },
+    {
+        Header: 'Concepto 2'
+    },
+    {
+        Header: 'Concepto 3'
+    },
+    {
+        Header: 'Concepto 4'
+    },
+    {
+        Header: 'Concepto 5'
+    },
+    {
+        Header: 'Concepto 6'
+    },
+    {
+        Header: 'Balance actual'
+    },
+    {
+        Header: 'Activo'
+    },
+    {
+        Header: 'Última venta'
+    },
+]
